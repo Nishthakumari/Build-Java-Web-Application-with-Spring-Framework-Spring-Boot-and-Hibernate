ac55e1a43b978614d6ac2706ae75b1a3dbb1b51c
Nishthakumari <knishtha0@gmail.com>
2023-04-17 16:37:40 +0530

    Add hard coded validation of userid and password

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -13,8 +13,11 @@ import java.sql.SQLOutput;
 @Controller
 public class LoginController {
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    private AuthenticationService authenticationService;
 
+    public LoginController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 
     @RequestMapping(value = "login", method = RequestMethod.GET)
     public String gotoLoginPage(){
@@ -24,8 +27,17 @@ public class LoginController {
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String gotoWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model){
-        model.put("name", name);
-        model.put("password", password);
-        return "welcome";
+
+        if(authenticationService.authenticate(name, password))
+        {
+            model.put("name", name);
+            model.put("password", password);
+            return "welcome";
+        }
+
+        model.put("errorMessage", "Inavalid Credential, Please try again!");
+
+        return "login";
+
     }
 }
