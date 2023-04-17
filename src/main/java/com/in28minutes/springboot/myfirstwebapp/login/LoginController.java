1cfd3ab0e93c9a851a6b7449f397b98e0fbd3769
Nishthakumari <knishtha0@gmail.com>
2023-04-17 19:05:16 +0530

    Adding JSTL to Spring Boot Project and Showing Todos in a Table

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -7,10 +7,12 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.sql.SQLOutput;
 
 @Controller
+@SessionAttributes("name")
 public class LoginController {
 
     private AuthenticationService authenticationService;
--- src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -5,7 +5,8 @@
 	<body>
 		<div> Welcome to in28minutes</div>
 		<div> Your name : ${name}</div>
-		<div> Your password : ${password}</div>
+		<div> <a href = "list-todos"> Manage</a> your todos</div>
+
 
 	</body>
 </html>
