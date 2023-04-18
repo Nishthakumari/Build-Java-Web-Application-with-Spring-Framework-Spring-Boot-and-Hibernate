d596cc0040947b71134fc2c12a5319f4f4f41b12
Nishthakumari <knishtha0@gmail.com>
2023-04-18 10:23:35 +0530

    Using Command Beans to implement New Todo Page Validations

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,7 +1,9 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,7 +43,12 @@ public class TodoController {
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodoPage( ModelMap model, Todo todo){
+    public String addNewTodoPage(ModelMap model, @Valid Todo todo, BindingResult result){
+        if(result.hasErrors())
+        {
+            return "todo";
+        }
+
 
         String username = (String)model.get("name");
 
